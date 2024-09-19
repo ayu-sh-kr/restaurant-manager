428365eb9a06a04ed60082a63840029b42d15224
ayu-sh-kr <akjaiswal2003@gmail.com>
2024-09-19 20:11:29 +0530

    Create OrderUseCases.kt
    contains use case related to order -> Creating Order and adding item to order

---
+++ src/main/kotlin/order/application/OrderUseCases.kt
@@ -0,0 +1,61 @@
+package org.example.order.application
+
+import org.example.UseCase
+import org.example.customer.domain.CustomerId
+import org.example.order.domain.Item
+import org.example.order.domain.ItemId
+import org.example.order.domain.Order
+import org.example.order.domain.OrderId
+import org.example.order.domain.OrderRepository
+
+@UseCase
+class OrderItemRemoveUseCase(private val orderRepository: OrderRepository) {
+
+
+    fun execute(orderId: OrderId, itemId: ItemId) {
+        val order = orderRepository.findById(orderId);
+        require(order != null, ({"Order was not found for given id: $orderId"}))
+        order.removeItem(itemId)
+        orderRepository.update(order, orderId)
+    }
+}
+
+@UseCase
+class OrderCreateUseCase(private val orderRepository: OrderRepository) {
+
+    fun execute(customerId: CustomerId, items: MutableList<Item>): Order {
+        val order = Order(
+            customerId = customerId,
+            items = items
+        )
+
+        return orderRepository.save(order)
+    }
+
+}
+
+@UseCase
+class OrderAddItemUseCase(private val orderRepository: OrderRepository) {
+
+    fun execute(item: Item, orderId: OrderId) {
+        val order = orderRepository.findById(orderId)
+        require(order != null, ({"Order was not found for given id: $orderId"}))
+        order.addItem(item)
+        orderRepository.update(order, orderId)
+    }
+}
+
+@UseCase
+class OrderFetchSingleUseCase(private val orderRepository: OrderRepository) {
+
+    fun execute(orderId: OrderId): Order? {
+        return orderRepository.findById(orderId)
+    }
+}
+
+class OrderFetchAllUseCase(private val orderRepository: OrderRepository) {
+
+    fun execute(): MutableList<Order> {
+        return orderRepository.findAll()
+    }
+}
